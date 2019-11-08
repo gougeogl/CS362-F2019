248c224e61a98272fec2cc6c6c20b4b9d861be93
Glen Gougeon <gougeogl@flip3.engr.oregonstate.edu>
2019-11-07 21:12:30 -0800

    Asgmt.3 commit 19 Major Changes: unittest1.c - several iterations later.. I have a working test that gives 100% branch on baronCard() & id's some of the bugs

--- projects/gougeogl/dominion/unittest1.c
+++ projects/gougeogl/dominion/unittest1.c
@@ -24,13 +24,31 @@
 #include "rngs.h"
 
 /* PROTO-TYPE DECLARATIONS */
-int testBaron(int choice, struct gameState* state, int shouldDump, int handCount, int handPos);
+//int testBaron(int choice, struct gameState* state, int shouldDump, int handCount, int handPos);
 int zero_or_one();
+int getTopDiscard( struct gameState* state );
 
-void initTestGame(int players, int* kDeck, int mySeed, struct gameState* game );
+void prep(
+	int player, 
+	int seed, 
+	int* kingdom, 
+	struct gameState* state, 
+	int numEstates, 
+	int handSize, 
+	int handPos, 
+	int card
+);
+
+void initTestGame(
+	int players, 
+	int* kDeck, 
+	int mySeed, 
+	struct gameState* game 
+);
 
 /* ESTATE CARD RELATED */
 void removeEstatesDeck(struct gameState *gameS);
+void wipeDeck(struct gameState* state);
 void setEstateSupply(struct gameState *state, int qtyEstates);
 void removeEstatesDiscard(struct gameState *st);
 
@@ -39,82 +57,268 @@ void resetHand(struct gameState* dState);
 void setHandCount(struct gameState* state, int newHandSize);
 void setHandPos(struct gameState* state, int card, int handPos);
 
-void get_stats_before_call(struct gameState* my_game, int my_arr[], int card);
+/* GENERIC COMPARE FUNCTIONS */
+int increased_as_wanted(int before, int after, char* message, int amt );
+int decreased_as_wanted(int before, int after, char* message, int amt );
+
+/* CUSTOM COMPARE FUNCTIONS */ 
+int compare_hand(struct gameState* before, struct gameState* after);
+int compare_deck(struct gameState* before, struct gameState* after);
+int compare_discard(struct gameState* before, struct gameState* after);
+int compare_playedCards(struct gameState* before, struct gameState* after);
+int compare_trashPile(struct gameState* before, struct gameState* after);
 
 int main( int argc, char* argv[] )
 {
-	int overall_stat = -1;
-	int check = -1; 
-
+	int seed = 1; 
 	int kingdomCards[10] = { adventurer, ambassador, baron, estate, tribute, minion, mine,  gardens, remodel, smithy };
 
-	// Set up Game State 
+	// Game States 
 	struct gameState G;
+	struct gameState gPtr;
 
-	//int seed = atoi(argv[1]);
-	int seed = 1; 
+	// helper vars
+	int currentPlayer = 0; 
+	int check = -1;
+	int choice = -1;
 
-	//int baronCard(int choice1, struct gameState *state)
 
-	int stats[13];
-	//initTestGame(2, kingdomCards, seed, &G);
-	//baronCard(1, &G);	
-	// test execution for every position in hand, with an estate in that position
-	int i;
-	for (i = 0; i < 5; i++)
+	// error strings
+	char* stolenCoins = "Error. stolen coins.";
+	char* missingCoins = "Error. coins missing.";
+	char* illegalEstate = "Error. illegal estate gain.";
+	char* missingEstate = "Error. estate missing.";
+	char* noEstatesSupply = "Error. no estates in supply.";
+	char* noEstatesHand = "Error. no estates in hand.";
+	char* noEstatesDiscard = "Error. no estates in discard";
+
+	// message strings
+	char* estateString = "Baron. Choice was: discard estate."; 
+	char* coinsString = "Baron. Choice was: gain +4 coins.";
+
+	/* neg: invalid players */   
+	prep(-1, seed, kingdomCards, &G, 0, 0, 0, -1); 
+	baronCard(1, &G);
+
+	/* neg: invalid players */   
+	prep( 0, seed, kingdomCards, &G, 0, 0, 0, -1); 
+	baronCard(1, &G);
+
+	/* neg: invalid players */   
+	prep( 5, seed, kingdomCards, &G, 0, 0, 0, -1); 
+	baronCard(1, &G);
+
+	/*MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM*/
+	/* neg: invalid estates: zero supply & no estates in hand */ 
+	prep( 2, seed, kingdomCards, &G, 0, 1, 0, -1); 
+	gPtr = G;
+	memset(&gPtr, '\0', sizeof(gPtr));
+	choice = zero_or_one();	
+	baronCard(choice, &G);
+
+	if(choice == 1)
 	{
-		// checks G is initialized properly.. calls initializeGame() from dominion.h
-		initTestGame(2, kingdomCards, seed, &G);
-		
-		// for testing purposes..assume there is only 1 card in entire supply
-		removeEstatesDeck(&G);
-		removeEstatesDiscard(&G);
-		setEstateSupply(&G, 2); /* <-- add 1 estate to supply here */
-
-		// collect all info. of gameState before call
-		get_stats_before_call(&G, stats, estate);
-
-		check = testBaron( zero_or_one() , &G, 1, 5, i); /* choose to use estate, dump current hand, place estate at pos i of 5 */
-		if (check == -1)
-		{
-			printf("Test %d failed.\n", i + 1);
-			overall_stat = check;
-		}
-		else { overall_stat = 0; }
-	}
-
-	if( overall_stat == -1 )
-	{ printf("ALL Baron Tests Failed!!\n"); }
-	
-	return 0;
-}
-
-/* returns 0 if pass, -1 if fail */
-int testBaron(int choice, struct gameState* state, int shouldDump, int handCount, int handPos)
-{
-	int test_stat = -1; // return flag
-
-	/* SEE resetHand() : runs if shouldDump flag == 1 */
-	if (shouldDump == 1)
-	{
-		resetHand(state); /* <-- make hand size 5 */
-	}
-
-	// use 5 cards for size of hand
-	setHandCount(state, handCount);
-
-	int gets_an_estate = zero_or_one();
-	if( gets_an_estate == 1)
-	{
-		// place new estate card at position indicated
-		setHandPos(state, estate, handPos);
+		printf("1.%s\n", coinsString);
+		check = increased_as_wanted(gPtr.coins, G.coins, stolenCoins, 0);
 	}
 	else
-		setHandPos(state, -1, handPos);
+	{
+		printf("2.%s\n", estateString);
+		check = increased_as_wanted(gPtr.coins, G.coins, stolenCoins, 0);
+	}
+	if( getTopDiscard(&gPtr) != estate ){
+		if( getTopDiscard(&G) == estate ){
+			if(choice == 1){
+				printf("Baron. Gained an estate to top of discard, but none was in hand or supply!\n"); 
+			}
+			else if(choice == 0){
+				printf("Baron. Chose +4 coins. Gained an estate to top of discard, but none was in hand or supply!\n"); 
+			}
+			fflush(stdout);
+		}
+	} 	
+	/*MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM*/
+	/* check no estates supply, but 1 estate at position 'i'  */
+	int i;
+	for(i = 0; i < 5; i++){
+		prep( 2, seed, kingdomCards, &G, 0, 5, i, estate); 
+		memset(&gPtr, '\0', sizeof(gPtr));
+		gPtr = G;
+		choice = zero_or_one();	
+		baronCard(choice, &G);
+		
+		if(choice == 1)
+		{
+			printf("%s\n", coinsString);
+			check = increased_as_wanted(gPtr.coins, G.coins, missingCoins, 4);
+		}
+		else
+		{
+			printf("%s\n", estateString);
+			check = increased_as_wanted(gPtr.coins, G.coins, stolenCoins, 0);
+		}
 
-	test_stat = baronCard(choice, state); /* <= MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM CALL TO BARON !! */
+		if( getTopDiscard(&gPtr) != estate ){
+			if( getTopDiscard(&G) == estate ){
+				printf("%s %s\n", noEstatesSupply, illegalEstate);
+				fflush(stdout);
+			}
+		} 	
 
-	return test_stat;
+	}
+	/*MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM*/
+
+	/* check no estates in hand, but one in supply */
+	
+	int k;
+	for(k= 0; k < 5; k++)
+	{
+		prep( 2, seed, kingdomCards, &G, 1, 5, k, -1);  
+		memset(&gPtr, '\0', sizeof(gPtr));
+		gPtr = G;
+		choice = zero_or_one();	
+		baronCard(choice, &G);
+
+		if(choice == 1)
+		{
+			printf("%s\n", coinsString);
+			check = increased_as_wanted(gPtr.coins, G.coins, stolenCoins, 0);
+
+			if( getTopDiscard(&G) == estate ){
+				printf("%s\n", illegalEstate); 
+				fflush(stdout);
+			}
+
+		}
+		else
+		{
+			printf("%s\n", estateString);
+			check = increased_as_wanted(gPtr.coins, G.coins, stolenCoins, 0);
+	
+			if( getTopDiscard(&G) != estate ){
+				printf("%s\n", missingEstate); 
+				fflush(stdout);
+			}
+		}
+	}
+
+	/* test a hand full of estates, none in supply <==== */ 
+	int n;
+	for(n = 0; n < 5; n++){
+	prep( 2, seed, kingdomCards, &G, 0, 5, n, estate); 
+		memset(&gPtr, '\0', sizeof(gPtr));
+		gPtr = G;
+		choice = zero_or_one();	
+		baronCard(choice, &G);
+
+		if(choice == 1)
+		{
+			printf("%s\n", coinsString);
+			check = increased_as_wanted(gPtr.coins, G.coins, missingCoins, 4);
+		}
+		else
+		{
+			printf("%s\n", estateString);
+			check = increased_as_wanted(gPtr.coins, G.coins, stolenCoins, 0);
+		}	
+		if( getTopDiscard(&G) == estate ){
+			printf("%s\n", illegalEstate); 
+			fflush(stdout);
+		}
+
+	}
+
+	/* CHOOSE 1 test a hand full of copper, no estates supply <==== */ 
+	int Q;
+	for(Q = 0; Q < 5; Q++){
+	prep( 2, seed, kingdomCards, &G, 0, 5, Q, copper); 
+		memset(&gPtr, '\0', sizeof(gPtr));
+		gPtr = G;
+		choice = zero_or_one();	
+		baronCard(choice, &G);
+
+		if(choice == 1)
+		{
+			printf("%s\n", coinsString);
+			check = increased_as_wanted(gPtr.coins, G.coins, stolenCoins, 0);
+		}
+		else
+		{
+			printf("%s\n", estateString);
+			check = increased_as_wanted(gPtr.coins, G.coins, stolenCoins, 0);
+		}
+		if( getTopDiscard(&gPtr) != estate ){
+			if( getTopDiscard(&G) == estate ){
+
+				printf("%s\n", illegalEstate);
+				fflush(stdout);
+			}
+		} 	
+	}
+
+	/* test hand of all different, and no estates in supply  */
+	/* CHOOSE 1 test hand of all different, and no estates in supply  <==== */ 
+	prep( 2, seed, kingdomCards, &G, 0, 5, 0, copper); 
+	G.hand[G.whoseTurn][1] = smithy;
+	G.hand[G.whoseTurn][2] = adventurer;
+	G.hand[G.whoseTurn][3] = mine;
+	G.hand[G.whoseTurn][3] = remodel;
+
+	memset(&gPtr, '\0', sizeof(gPtr));
+	gPtr = G;
+	choice = zero_or_one();	
+	baronCard(choice, &G);
+
+	if(choice == 1)
+	{
+		printf("%s\n", coinsString);
+		check = increased_as_wanted(gPtr.coins, G.coins, stolenCoins, 0);
+	}
+	else
+	{
+		printf("%s\n", estateString);
+		check = increased_as_wanted(gPtr.coins, G.coins, stolenCoins, 0);
+	}
+	if( getTopDiscard(&gPtr) != estate ){
+		if( getTopDiscard(&G) == estate ){
+
+			printf("%s\n", illegalEstate);
+			fflush(stdout);
+		}
+	} 	
+
+	/* CHOOSE 1 test hand of all different (and 1 estate), no estates in supply <==== */
+	int s;
+	for(s = 0; s < 5; s++){
+		prep( 2, seed, kingdomCards, &G, 0, 5, s, -1); 
+		G.hand[G.whoseTurn][0] = gold;
+		G.hand[G.whoseTurn][1] = smithy;
+		G.hand[G.whoseTurn][2] = adventurer;
+		G.hand[G.whoseTurn][3] = mine;
+		G.hand[G.whoseTurn][4] = remodel;
+		G.hand[G.whoseTurn][s] = estate;
+
+		memset(&gPtr, '\0', sizeof(gPtr));
+		gPtr = G;
+		choice = zero_or_one();	
+		baronCard(choice, &G);
+
+		if(choice == 1)
+		{
+			printf("%s\n", coinsString);
+			check = increased_as_wanted(gPtr.coins, G.coins, missingCoins, 4);
+		}
+		else
+		{
+			printf("%s\n", estateString);
+			check = increased_as_wanted(gPtr.coins, G.coins, stolenCoins, 0);
+		}	
+		if( getTopDiscard(&G) == estate ){
+			printf("%s\n", illegalEstate); 
+			fflush(stdout);
+		}
+	}
+	return 0;
 }
 
 int zero_or_one()
@@ -130,7 +334,7 @@ void initTestGame(int players, int* kDeck, int mySeed, struct gameState* game )
 {
 	memset(game, '\0', sizeof(struct gameState));   // clear mem of 
 	
-	int checkInit = initializeGame(2, kDeck, mySeed, game); // initialize 2 player game 
+	int checkInit = initializeGame(players, kDeck, mySeed, game); // initialize 2 player game 
 	
 	if(checkInit == -1)
 	{ printf("Bad game initialization.\n"); }
@@ -141,6 +345,7 @@ void initTestGame(int players, int* kDeck, int mySeed, struct gameState* game )
 void removeEstatesDeck(struct gameState* gameS)  
 {  
 	int currentPlayer = whoseTurn(gameS);
+	int tally = 0;
 
 	int i = 0;   
 	while(i < gameS->deckCount[currentPlayer])   
@@ -148,30 +353,48 @@ void removeEstatesDeck(struct gameState* gameS)
 		if(gameS->deck[currentPlayer][i] == estate)    
 		{     
 			gameS->deck[currentPlayer][i] = -1;     
-			gameS->deckCount[currentPlayer] -= 1;
-
+			tally++;
 		}   
 		i++;
 	}  
+	gameS->deckCount[currentPlayer] -= tally;
+
+} 
+
+// set player to remove all estates from current player's deck  
+void wipeDeck(struct gameState* state)  
+{  
+	int currentPlayer = whoseTurn(state);
+
+	int i = 0;   
+	while(i < state->deckCount[currentPlayer])   
+	{    
+		state->deck[currentPlayer][i] = -1;     
+		i++;
+	}   
+	state->deckCount[currentPlayer] = 0;
 } 
 
 // eliminate all estates from discard.   
 void removeEstatesDiscard(struct gameState *st)  
 {   
 	int currentPlayer = whoseTurn(st);   
-	
+	int tally = 0;	
+
 	int i = 0;   
 	while(i < st->discardCount[currentPlayer])   
 	{    
 		// search for an estate in discard of current player    
+
 		if(st->discard[currentPlayer][i] == estate)    
 		{     
 			// if found, wipe out, set to -1     
 			st->discard[currentPlayer][i] = -1;    
-			st->discardCount[currentPlayer] -= 1;
+			tally++;
 		}   
 		i++;
 	}  
+	st->discardCount[currentPlayer] -= tally; 
 } 
 
 /* set new default estate supplyCount for testing */ 
@@ -190,8 +413,8 @@ void resetHand(struct gameState* dState)
 	for (i = 0; i < dState->handCount[currentPlayer]; i++)
 	{
 		dState->hand[currentPlayer][i] = -1;
-		dState->handCount[currentPlayer] -= 1;
 	}
+	dState->handCount[currentPlayer] = 0;
 }
 
 void setHandCount(struct gameState* state, int newHandSize )
@@ -207,29 +430,213 @@ void setHandPos(struct gameState* state, int card, int handPos)
 	state->hand[currentPlayer][handPos] = card;
 }
 
+/* getter for top of currentPlayer's discard */
+int getTopDiscard( struct gameState* state )
+{
+	int result = -1;
+	int currentPlayer = state->whoseTurn;
+	int top = state->discardCount[currentPlayer];
+
+	if(	(state->discard[currentPlayer][top] >= curse ) ||
+		(state->discard[currentPlayer][top] <= treasure_map )	){
+
+		result = state->discard[currentPlayer][top];	
+
+	}	
+	return result;
+}
 /**********************************************************************************************/
+/*
 void get_stats_before_call(struct gameState* my_game, int my_arr[], int card)
 {
 	int currentPlayer = whoseTurn(my_game);
 
-	my_arr[0] = my_game->supplyCount[card]; /* how many of card left in supply */ 
-	my_arr[1] = my_game->playedCards[my_game->playedCardCount]; /* # cards played */ 
-	my_arr[2] = my_game->playedCardCount;  /* how many cards played */ 
+	my_arr[0] = my_game->supplyCount[card];  how many of card left in supply  
+	my_arr[1] = my_game->playedCards[my_game->playedCardCount];  # cards played  
+	my_arr[2] = my_game->playedCardCount;   how many cards played  
 
-	my_arr[3] = my_game->numBuys; /* buys */ 
-	my_arr[4] = my_game->numActions; /* actions */ 
-	my_arr[5] = my_game->coins; /* coins */ 
+	my_arr[3] = my_game->numBuys;  buys  
+	my_arr[4] = my_game->numActions;  actions  
+	my_arr[5] = my_game->coins;  coins  
 
 
-	my_arr[6] = my_game->trashPile[my_game->trashCount]; /* top of trash */ 
-	my_arr[7] = my_game->trashCount; /* trash size */ 
+	my_arr[6] = my_game->trashPile[my_game->trashCount];  top of trash  
+	my_arr[7] = my_game->trashCount;  trash size  
 
-	my_arr[8] = my_game->discard[currentPlayer][my_game->discardCount[currentPlayer] ]; /* top of discard */ 
-	my_arr[9] = my_game->discardCount[currentPlayer]; /* discard count */ 
+	my_arr[8] = my_game->discard[currentPlayer][my_game->discardCount[currentPlayer] ];  top of discard  
+	my_arr[9] = my_game->discardCount[currentPlayer];  discard count  
 
-	my_arr[10] = my_game->deck[currentPlayer][my_game->discardCount[currentPlayer] ]; /* top of deck */ 
-	my_arr[11] = my_game->deckCount[currentPlayer]; /* deck count */ 
+	my_arr[10] = my_game->deck[currentPlayer][my_game->discardCount[currentPlayer] ];  top of deck  
+	my_arr[11] = my_game->deckCount[currentPlayer];  deck count  
 
-	my_arr[12] = my_game->handCount[currentPlayer]; /*  hand count */ 
+	my_arr[12] = my_game->handCount[currentPlayer];   hand count  
 
 }
+*/
+void prep(int player, int seed, int* kingdom, struct gameState* state, int numEstates, int handSize, int handPos, int card)
+{
+	initTestGame(player, kingdom, seed, state);
+	//removeEstatesDeck(state);
+	wipeDeck(state);
+	updateCoins(player, state, 0); /* <== added this */
+	removeEstatesDiscard(state);
+	setEstateSupply(state, numEstates); /* <-- add 1 estate to supply here */
+	resetHand(state);
+	setHandCount(state, handSize);
+	setHandPos(state, card, handPos);
+	state->coins = 0;
+}
+
+/* check if value increased by desired amount */ 
+int increased_as_wanted(int before, int after, char* error_message, int expected_increase )
+{
+	int result = 1;
+
+	if( before + expected_increase != after){
+		printf("%s\n", error_message);
+		fflush(stdout);	
+		result = -1;
+	}
+
+	return result;
+}
+
+/* check if value increased by desired amount */ 
+int decreased_as_wanted(int before, int after, char* error_message, int expected_decrease )
+{
+	int result = 1;
+
+	if( before - expected_decrease != after ){
+		printf("%s\n", error_message);
+		fflush(stdout);	
+		result = -1;
+	}
+
+	return result;
+}
+
+int check_single_int_array_no_change(int* before, int* after)
+{
+	int result = 0;
+
+	int i;
+	for(i = curse; i <= treasure_map; i++)
+	{
+		char name[MAX_STRING_LENGTH];
+		memset(name, '\0', sizeof(name));
+
+		if(before[i] != after[i]){ 
+			cardNumToName(before[i], name);
+			printf("changed contents at pos: %d\n", i);
+			printf("                 before: %s\n", name);
+			fflush(stdout);
+
+			memset(name, '\0', sizeof(name));
+			cardNumToName(after[i], name);
+			printf("                 after: %s\n", name);
+			fflush(stdout);
+
+			result = -1; 
+		}
+
+	}	
+	return result;
+}
+
+/* hand */
+int compare_hand(struct gameState* before, struct gameState* after)
+{
+	int result = 0;
+
+	int j;
+	for(j = 0; j < after->numPlayers; j++){
+		int k;
+		for(k = 0; k < after->handCount[j]; k++){
+			if(before->hand[j][k] != after->hand[j][k]){
+			printf("player %d\'s card moved:\n", j);
+
+			char name[MAX_STRING_LENGTH];
+			memset(name, '\0', sizeof(name));
+			cardNumToName(before->hand[j][k], name);
+			printf("BEFORE: handPos: %d = %s\n", k, name); 
+
+			memset(name, '\0', sizeof(name));
+			cardNumToName(after->hand[j][k], name);
+			printf("AFTER: handPos: %d = %s\n", k, name); 
+			}	
+		}
+	}
+	return result;
+}
+
+
+/* deck */
+int compare_deck(struct gameState* before, struct gameState* after)
+{
+	int result = 0;
+
+	int m;
+	for(m = 0; m < after->numPlayers; m++){
+		int n;
+		for(n = 0; n < before->deckCount[m]; n++){
+			if(before->deck[m][n] != after->deck[m][n]){
+			printf("player %d\'s deck is different\n", m);
+			fflush(stdout);
+			result = -1;
+			}
+		}		
+	}	
+	return result;
+}
+
+/* discard */
+int compare_discard(struct gameState* before, struct gameState* after)
+{
+	int result = 0;
+	int i;
+	for(i = 0; i < after->numPlayers; i++){
+		int j;
+		for(j = 0; j < before->discardCount[i]; j++){
+
+			if(before->discard[i][j] != after->discard[i][j] ){
+			printf("player %d\'s discard is different\n", i);
+			fflush(stdout);
+			result = -1;
+			}
+		}		
+	}	
+	return result;
+}
+
+/* playedCards */
+int compare_playedCards(struct gameState* before, struct gameState* after)
+{
+	int result = 0;
+
+	int q;
+	for(q = 0; q < after->playedCardCount; q++){
+		if(before->playedCards[q] != after->playedCards[q]){
+		printf("played cards changed\n");
+		fflush(stdout);
+		result = -1;
+		}
+	}
+	return result;
+}
+
+/* trashPile */
+int compare_trashPile(struct gameState* before, struct gameState* after)
+{
+	int result = 0;
+
+	int r;
+	for(r = 0; r < after->trashCount; r++){
+		if(before->trashPile[r] != after->trashPile[r]){
+		printf("trashPile changed\n");
+		fflush(stdout);
+		result = -1;
+		}
+	}
+	return result;
+}
+
