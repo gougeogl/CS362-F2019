1cc9016b6f59081eca8647ebc1b9b55a71a1409e
Glen Gougeon <gougeogl@oregonstate.edu>
2019-11-11 12:31:30 -0800

    FinalProject: commit 1
    Contents:
    1. Original Dominion Code Base EXCEPT
    Makefile modified from original
    2. unittest_template.c
    * used in Part B

---
+++ projects/FinalProject-Bugs/dominion/unittest_template.c
@@ -0,0 +1,32 @@
+/*****************************************************************
+* FileName:		unittest_template.c
+* Author:		Glen Gougeon
+* Group:		EvenBetter
+* Members:		Samnang Penh, Reuben Youngblom, & Author
+* GitHubRepo:	tbd
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* Class:		CS362 Software Engineering II
+* Date:			11-11-2019
+* Last Mod:		11-11-2019
+
+* Assignment:	Final Project Part B: 
+				Dominion Unit Test/Fix Bugs
+
+* Description:	Template for Unit Testing Bugs
+
+******************************************************************/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+//#include "interface.h" <==== This has a lot of great 'prints' useful for checking states
+#include "rngs.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	return 0;
+}
+/* ** EOF ** /
